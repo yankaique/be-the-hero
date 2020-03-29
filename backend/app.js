5ab7adf1645cd309f020c8cd2112ec0bda5f96ff
Yan <yankaiquecosta.yk@gmail.com>
2020-03-29 09:23:07 -0300

    Autenticação e testes Back-end

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-
+const {errors} = require('celebrate');
 const app = express();
 const routes = require('./src/routes');
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.listen(3333);
+app.use(errors());
+
+
+module.exports = app;
---
+++ backend/src/server.js
@@ -0,0 +1,5 @@
+const app = require('../app');
+
+app.listen(3333,function(){
+    console.log("server is running on port 5010");
+ });
